b72e32fc9e66f6ecd629e13cdf35df2430eadb00
wukrit <sukritwalia@gmail.com>
2019-09-10 14:01:29 -0400

    Implemented player search and started player sort

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,15 @@
 class PlayersController < ApplicationController
 
   def index
-    @teams = Team.all
+    @teams = Team.alphabetical
+    if params[:search_name]
+      @players = Player.where("name LIKE ?", "%#{params[:search_name].titleize}%")
+      @teams = @teams.select do |team|
+        @players.pluck(:team).include?(team.code)
+      end
+    else
+      @players = Player.all
+    end
   end
 
   def show
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,9 @@
 class Team < ApplicationRecord
 
+  def self.alphabetical
+    self.all.order(:name)
+  end
+
   def find_players
     Player.all.select do |player|
       player.team == self.code
