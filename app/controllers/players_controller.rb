f1b5786df60835ba3616c6743f2d87fd0047f1f9
wukrit <sukritwalia@gmail.com>
2019-09-10 19:50:04 -0400

    Added clear filters

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,26 +4,38 @@ class PlayersController < ApplicationController
     # Use cookie to store filter and search data until user clears it
 
     @teams = Team.alphabetical
+    session[:search_name] ||= params[:search_name]
+    session[:filter] ||= params[:filter]
+    session[:filter_option] ||= params[:filter_option]
 
-    if params[:search_name]
-      @players = Player.where("name LIKE ?", "%#{params[:search_name].titleize}%")
+    if session[:search_name]
+      @players = Player.where("name LIKE ?", "%#{session[:search_name].titleize}%")
       @teams = @teams.where(code: @players.pluck(:team))
     else
       @players = Player.all
     end
 
-    if params[:filter_option]
-      if params[:filter] == "position"
-        @players = @players.where(position: params[:filter_option])
+    if session[:filter_option]
+      if session[:filter] == "position"
+        @players = @players.where(position: session[:filter_option])
+        @teams = @teams.where(code: @players.pluck(:team))
       else
         @teams = Team.alphabetical
-        @teams = @teams.where(code: params[:filter_option])
+        @teams = @teams.where(code: session[:filter_option])
       end
     end
 
   end
 
+  def clear
+    session[:search_name] = nil
+    session[:filter_option] = nil
+    session[:filter] = nil
+    redirect_to players_path
+  end
+
   def show
     @player = Player.find(params[:id])
   end
+
 end
