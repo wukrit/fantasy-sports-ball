22f80b0f44730e99073ed2255af65dfafbe29a14
mrwerner392 <mrwerner392@gmail.com>
2019-09-12 12:22:53 -0400

    Refactor player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,30 +1,10 @@
 class PlayersController < ApplicationController
+  before_action :initialize_search, only: :index
 
   def index
     # Use cookie to store filter and search data until user clears it
-
-    @teams = Team.alphabetical
-    session[:search_name] ||= params[:search_name]
-    session[:filter] = params[:filter]
-    params[:filter_option] = nil if params[:filter_option] == ""
-    session[:filter_option] = params[:filter_option]
-
-    if session[:search_name]
-      @players = Player.where("name LIKE ?", "%#{session[:search_name].titleize}%")
-      @teams = @teams.where(code: @players.pluck(:team))
-    else
-      @players = Player.all
-    end
-
-    if session[:filter_option]
-      if session[:filter] == "position"
-        @players = @players.where(position: session[:filter_option])
-        @teams = @teams.where(code: @players.pluck(:team))
-      else
-        @teams = @teams.where(code: session[:filter_option])
-      end
-    end
-
+    handle_search_name
+    handle_filters
   end
 
   def clear
@@ -34,6 +14,36 @@ class PlayersController < ApplicationController
 
   def show
     @player = Player.find(params[:id])
+  end
+
+  private
+
+  def initialize_search
+    @teams = Team.alphabetical
+    session[:search_name] ||= params[:search_name]
+    session[:filter] = params[:filter]
+    params[:filter_option] = nil if params[:filter_option] == ""
+    session[:filter_option] = params[:filter_option]
+  end
+
+  def handle_search_name
+    if session[:search_name]
+      @players = Player.where("name LIKE ?", "%#{session[:search_name].titleize}%")
+      @teams = @teams.where(code: @players.pluck(:team))
+    else
+      @players = Player.all
+    end
+  end
+
+  def handle_filters
+    if session[:filter_option]
+      if session[:filter] == "position"
+        @players = @players.where(position: session[:filter_option])
+        @teams = @teams.where(code: @players.pluck(:team))
+      else
+        @teams = @teams.where(code: session[:filter_option])
+      end
+    end
   end
 
 end
