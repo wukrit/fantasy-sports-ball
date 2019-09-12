8b720caa33907a9a0ccb86c8a4b048418e8f87b7
mrwerner392 <mrwerner392@gmail.com>
2019-09-12 10:18:48 -0400

    Sort roster show page

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -3,5 +3,14 @@ class Roster < ApplicationRecord
   has_many :roster_players, dependent: :destroy
   has_many :players, through: :roster_players
 
+  def sorted_players
+    order = ["QB1", "RB1", "RB2", "WR1", "WR2", "TE", "DEF", "K"]
+    sorted_players = []
+    order.each do |position|
+      sorted_players << self.roster_players.find_by(roster_position: position)
+    end
+    sorted_players
+  end
+
   # accepts_nested_attributes_for :roster_players
 end
